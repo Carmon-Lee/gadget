3a90c257109f5c092641c79be52020e065dcf1a6
guang.li <liguang@xsyxsc.com>
2020-10-10 09:30:35 +0800

    dp

--- leetcode/src/main/java/dp/palindrone/LongestPalindroneSubsequence.java
+++ leetcode/src/main/java/dp/palindrone/LongestPalindroneSubsequence.java
@@ -6,5 +6,16 @@ package dp.palindrone;
  */
 public class LongestPalindroneSubsequence {
 
+    //求最长回文子序列的长度
+    public int longestPalindroneLength(String s) {
+        return 0;
+    }
+
+
+    // 求最长回文串的字符串表示
+    // 核心思想是先将原有字符串逆序，然后将得到的逆序和原有字符串进行比较，获取最长子串即可，思路非常巧妙
+    public String longestPalindroneStr(String s) {
+        return "";
+    }
 
 }
